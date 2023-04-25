6159bf3d3525dd0fa57d183169e7267b2d5178de
Alex Sitan <sitan.pentester.oscp@gmail.com>
2023-04-25 00:59:10 -0600

    Version 1.0.00

--- ProyectOne.sh
+++ ProyectOne.sh
@@ -11,6 +11,14 @@ unconfirmed_transactions="https://www.blockchain.com/es/explorer/mempool/btc"
 inspect_transaction_url="https://www.blockchain.com/explorer/transactions/btc/"
 inspect_transaction_adress="https://www.blockchain.com/explorer/addresses/btc"
 
+trap ctrl_c INT
+
+function ctrl_c(){
+	echo -e "\n\tSALIENDO......\n"
+	rm ut.tmp
+	rm ut.table
+	exit 0
+}
 
 
 helpPanel(){
@@ -20,21 +28,40 @@ helpPanel(){
         echo -e "\t\t${purpleColour}unconfirmed_transaction${endColour}\t${greenColour}Listar transacciones no confirmadas${endColour}"
         echo -e "\t\t${purpleColour}inspect${endColour}\t${greenColour}Inspeccionar hash de una transacciom${endColour}"
         echo -e "\t\t${purpleColour}adress${endColour}\t${greenColour}Inspeccionar direccion de una transaccion ${endColour}"
-        echo -e "\t${naranjaColour}[-h] ${endColour}${skyblueColour}Mostrar panel de ayuda${endColour}"
+        echo -e "\t${naranjaColour}[-n]${endColour}\t${purpleColour}Mostrar Numero De Lineas.${endColour}${greenColour} Ejemplo: -n 10${endColour}\n"
+				echo -e "\t${naranjaColour}[-h] ${endColour}${skyblueColour}Mostrar panel de ayuda${endColour}"
         echo -e "\n\n"
         exit 1
 }
 
 
 function unconfirmedTransaction(){
-    echo -e "\n\tListando ....\n"
+	number_output=$1
+	echo '' > ut.tmp
+	while [ "$(cat ut.tmp | wc -l)" == "1" ]; do
+		curl -s "$unconfirmed_transactions" | html2text > ut.tmp
+	done
+	hashes=$(cat ut.tmp | grep "-"  | awk 'NF{print $NF}'| grep -v '>'| grep "\-" | tail -n $number_output)
+	for i in $(seq 1 69); do echo -ne "*"; done;
+	echo -e "\n| HASHES\tTIEMPO\t\tBITCOINS\t\tCANTIDAD    |"
+	for i in $(seq 1 69); do echo -ne "*"; done;
+	echo -e "\n|"
+	for hash in $hashes;do
+		echo -e "| ${hash}\t$(cat ut.tmp | grep "$hash" -A 1    | tail -n 1 | awk 'NF{print $NF}')\t$(cat    ut.tmp | grep "$hash" -A 2 | tail -n 1)\t\t$(cat ut.tmp | grep "$hash" -A 3 | tail -n 1)" >> ut.table
+	done
+	cat ut.table
+	for i in $(seq 1 69); do echo -ne "*"; done; echo "|"
+	sleep 2
+#	rm ut.*
 }
 
 
-declare -i parameter_counter=0;while getopts "i:e:h" arg; do
+
+declare -i parameter_counter=0;while getopts ":n:e:h" arg; do
     case $arg in
         e) exploration_mode=$OPTARG;let parameter_counter+=1;;
-        h) helpPanel;;
+				n) number_output=$OPTARG; let parameter_counter+=1;;
+				h) helpPanel;;
     esac
 done
 
@@ -42,6 +69,11 @@ if [ $parameter_counter -eq 0 ]; then
     helpPanel
 else
     if [ "$(echo $exploration_mode)" == "unconfirmed_transaction" ]; then
-        echo -e "\n\tLISTANDO....\n"
+			if [ ! "$number_output" ]; then
+				number_output=100
+				unconfirmedTransaction $number_output
+			else
+				unconfirmedTransaction $number_output
+			fi
     fi
 fi
